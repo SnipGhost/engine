4f18e15586538dc0b33b5f083064b8c2208c43ed
HeliSRaiN <lhelisrainl@gmail.com>
2017-07-06 22:10:00 +0300

    Fix event system

--- projectTMG/Engine/event.cpp
+++ projectTMG/Engine/event.cpp
@@ -5,25 +5,45 @@ using namespace ng;
 //-----------------------------------------------------------------------------
 bool Event::isKeyboardKey(sf::Keyboard::Key keyboard)
 {
-	if(type == KeyReleased && key.code == keyboard) return true;
+	static bool flag = true;
+	if(flag == true && type == KeyPressed && key.code == keyboard) 
+	{
+			flag = false;
+			return true;
+	}
+
+	if(type == KeyReleased && key.code == keyboard) 
+		flag = true;
+
 	return false;
 }
 //-----------------------------------------------------------------------------
 bool Event::isWinClosed()
 {
-	if(type == sf::Event::Closed) return true;
+	if(type == sf::Event::Closed) 
+		return true;
 	return false;
 }
 //-----------------------------------------------------------------------------
 bool Event::isMouseKey(sf::Mouse::Button mouse)
 {
-	if (type == MouseButtonReleased && mouseButton.button == mouse) return true;
+	static bool flag = true;
+	if(flag == true && type == MouseButtonPressed && mouseButton.button == mouse) 
+	{
+		flag = false;
+		return true;
+	}
+
+	if(type == MouseButtonReleased && mouseButton.button == mouse) 
+		flag = true;
+
 	return false;
 }
 //-----------------------------------------------------------------------------
 bool Event::isMusicPlay(sf::Music &music)
 {
-	if(music.getStatus() == sf::Music::Playing) return true;
+	if(music.getStatus() == sf::Music::Playing) 
+		return true;
 	return false;
 }
 //-----------------------------------------------------------------------------
