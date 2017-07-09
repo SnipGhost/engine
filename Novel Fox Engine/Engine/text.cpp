ac4c8ba2ca259bd7171a4bb615d9ce24880a8f5e
HeliSRaiN <lhelisrainl@gmail.com>
2017-07-09 15:14:05 +0300

    Fix text output

--- Novel Fox Engine/Engine/text.cpp
+++ Novel Fox Engine/Engine/text.cpp
@@ -13,49 +13,29 @@ Text::Text(TextData td)
 	setText(td.text, td.color, td.x, td.y, td.size);
 }
 //-----------------------------------------------------------------------------
-//std::wstring wide_string(std::string const &s, std::locale const &loc)
-//{
-//	if (s.empty())
-//		return std::wstring();
-//	std::ctype<wchar_t> const &facet = std::use_facet<std::ctype<wchar_t> >(loc);
-//	char const *first = s.c_str();
-//	char const *last = first + s.size();
-//	std::vector<wchar_t> result(s.size());
-//
-//	facet.widen(first, last, &result[0]);
-//
-//	return std::wstring(result.begin(), result.end());
-//}
-//-----------------------------------------------------------------------------
-//std::wstring char2wchar(const std::string & p_in, const std::locale & p_loc)
-//{
-//	try {
-//		std::wstring out(p_in.length(), 0);
-//		std::string::const_iterator i = p_in.begin(), ie = p_in.end();
-//		std::wstring::iterator j = out.begin();
-//		for (; i != ie; ++i, ++j)
-//			*j = std::use_facet< std::ctype< wchar_t > >(p_loc).widen(*i);
-//		return out;
-//	}
-//	catch (...)
-//	{
-//		return std::wstring();
-//	}
-//};
+std::wstring wide_string(std::string const &s, std::locale const &loc)
+{
+	if (s.empty())
+		return std::wstring();
+	std::ctype<wchar_t> const &facet = std::use_facet<std::ctype<wchar_t> >(loc);
+	char const *first = s.c_str();
+	char const *last = first + s.size();
+	std::vector<wchar_t> result(s.size());
+
+	facet.widen(first, last, &result[0]);
+
+	return std::wstring(result.begin(), result.end());
+}
 //-----------------------------------------------------------------------------
 bool Text::setText(std::string text, std::string color, float x, float y, unsigned int size)
 {
 	font.loadFromFile("Resources/font.ttf");
 
-	/*char const *russian_locale_designator = "rus";
-	std::locale loc(russian_locale_designator);
-	std::wstring const textR = char2wchar(text, loc);
-	std::locale::global(loc);*/
+	std::locale loc("rus");
+	std::wstring const textR = wide_string(text, loc);
+	std::locale::global(loc);
 
-	//std::string str(text);
-	//std::wstring wstr(str.begin(), str.end());
-
-	setString(text); //_T _TEXT !!!
+	setString(textR);
 	setFont(font);
 
 	mapping["red"] = 1;
