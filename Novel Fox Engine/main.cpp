6c195ba8e7c5df9c25e6bf8e49e5e45f4a8e1a79
HeliSRaiN <lhelisrainl@gmail.com>
2017-07-11 02:15:27 +0300

    fix text output

--- Novel Fox Engine/main.cpp
+++ Novel Fox Engine/main.cpp
@@ -2,7 +2,6 @@
 // main.cpp														 DEMO-Программа
 //-----------------------------------------------------------------------------
 #include "Engine/engine.hpp"
-#include "Modules/primitiveGui.h"
 //-----------------------------------------------------------------------------
 using namespace ng;
 //-----------------------------------------------------------------------------
@@ -79,63 +78,16 @@ int main()
 	kernel.print("Resources loaded.", NORM);
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	pProgressBar progressBar(50, 20, 200, 50);
-	pSlider slider(50, 100, 200, 40);
-
-	pButton button1(400, 20, 200, 50);
-	button1.setText(sf::String("Show/Hide Window 1"));
-
-	pWindow window1(400, 250, 300, 300);
-	window1.setWindowTitle(sf::String("Window No. 1"));
-	pButton onWindow1Button(20, 20, 200, 40);
-	onWindow1Button.setText(sf::String("Change text"));
-	pTextEdit onWindow1TextEdit(20, 80, 200, 40);
-
-	window1.addWidget(&onWindow1Button);
-	window1.addWidget(&onWindow1TextEdit);
-
-	pWindow window2(50, 250, 250, 300);
-	window2.setWindowTitle(sf::String("Window No. 2"));
-	pCheckBox onWindow2CheckBox(30, 30, 200, 30);
-	onWindow2CheckBox.setText(sf::String("Check me :)"));
-
-	window2.addWidget(&onWindow2CheckBox);
-
-	pGui gui;
-	gui.addWidget(&progressBar);
-	gui.addWidget(&slider);
-	gui.addWidget(&button1);
-
-	gui.addWindow(&window1);
-	gui.addWindow(&window2);
-	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 	while (kernel.window->isOpen())
 	{
 		Event event;
 		while (kernel.window->pollEvent(event))
 		{
-			//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			gui.update(event, *kernel.window);
-
-			if (onWindow1Button.widgetEvent == pWidget::WidgetClicked)
-				onWindow1TextEdit.setText(sf::String("This is new text"));
-
-			if (button1.widgetEvent == pWidget::WidgetClicked)
-				if (window1.visiblity)
-					window1.setVisiblity(false);
-				else
-					window1.setVisiblity(true);
-			//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 			if (event.isKeyboardKey(event.keyboard.Escape) ||
 				event.isWinClosed()) kernel.window->close();
 			if (event.isMouseClickKey(sf::Mouse::Left)) sound.play();
 		}
 		if (event.isMouseKey(sf::Mouse::Right)) music.setStop();
-
-		progressBar.setValue(slider.getValue()); //[!]
 
 		if (event.type == sf::Event::LostFocus || !kernel.window->hasFocus())
 		{ 
@@ -158,8 +110,6 @@ int main()
 
 		for (ObjIt it = objects.begin(); it != objects.end(); ++it)
 			it->second->display();
-
-		kernel.window->draw(gui); //[!]
 
 		endDisplay();
 	}
