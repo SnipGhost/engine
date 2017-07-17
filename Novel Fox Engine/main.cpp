277b7404b313b423cb97c283eb2d204b06e92737
SnipGhost <snipghost@list.ru>
2017-07-17 23:25:57 +0300

    Вынес пример отдельно

--- Novel Fox Engine/main.cpp
+++ Novel Fox Engine/main.cpp
@@ -5,6 +5,27 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ng::Kernel &kernel = ng::Kernel::init(); // Для наглядности получаем свою ссыль
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void example(ng::Scene &scene)
+{
+	if (scene.objects.count("rain") > 0)
+		scene.objects["rain"]->visible = false;
+
+	// На самом деле, музыка почему-то не стопится
+	// Поэтому я её тупо удалил ... TODO: FIX! [?]
+
+	if (scene.music.count("music3") > 0)
+	{
+		delete scene.music["music3"];
+		scene.music.erase("music3");
+	}
+
+	if (scene.music.count("music2") > 0)
+	{
+		delete scene.music["music2"];
+		scene.music.erase("music2");
+	}
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int main()
 {
 	ng::Scene scene;
@@ -12,25 +33,9 @@ int main()
 	
 	while (kernel.window->isOpen())
 	{
-		if (kernel.checkEvents()) // Пример события: прошел дождь
+		if (kernel.checkEvents()) // TODO: Возвращать не bool, а enum
 		{
-			if (scene.objects.count("rain") > 0)
-				scene.objects["rain"]->visible = false;
-
-			// На самом деле, музыка почему-то не стопится
-			// Поэтому я её тупо удалил ... TODO: FIX! [?]
-
-			if (scene.music.count("music3") > 0)
-			{
-				delete scene.music["music3"];
-				scene.music.erase("music3");
-			}
-
-			if (scene.music.count("music2") > 0)
-			{
-				delete scene.music["music2"];
-				scene.music.erase("music2");
-			}
+			example(scene);       // Пример события: прошел дождь
 		}
 
 		if (kernel.lostFocus())
